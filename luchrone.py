3ccef5d1833954d069afbb7717c4c629eb10a0a5
Nicolas Ganivet <nicolas@cividesk.com>
2021-09-11 14:23:34 -0600

    initial commit

---
+++ luchrone.py
@@ -0,0 +1,106 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Simple test for NeoPixels on Raspberry Pi
+import time
+import board
+import neopixel
+
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+CUBE_SIZE = 5
+TAIL_SIZE = 15
+TAIL_START = CUBE_SIZE * CUBE_SIZE * CUBE_SIZE
+num_pixels = TAIL_START + TAIL_SIZE
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+
+# draw functions:
+# cube.dot(x, y, z)
+# cube.cube(x, y, z, w) - w=1 dot, w=2+: full cube
+# cube.line(x1, y1, z1, x2, y2, z2) - lines, 1 px large
+# cube.fill(x1, y1, z1, x2, y2, z2) - lines and planes
+
+# cube.dot:
+# ignore if out of bounds (0-4)
+# 149 - (since starting from end of string)
+# planes are weaved to exit at opposite ends, so even/odd
+# if (z is even)
+#   if (y is even)
+#     p = (y * 5) + x
+#   else
+#     p = (y * 5) + 4-x
+# else
+#   if (x is even)
+#     p = ((4-x) * 5) + (4-y)
+#   else
+#     p = ((4-x) * 5) + y
+# p = p + z * 25
+
+
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+
+while True:
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((255, 0, 0))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((255, 0, 0, 0))
+    pixels.show()
+    time.sleep(1)
+
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((0, 255, 0))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((0, 255, 0, 0))
+    pixels.show()
+    time.sleep(1)
+
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((0, 0, 255))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((0, 0, 255, 0))
+    pixels.show()
+    time.sleep(1)
+
+    rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
